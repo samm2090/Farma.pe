5ad8458d406951a3fabf533bf9179a8354609629
INTEGRAZI EIRL <INTEGRAZI EIRL>
2016-06-15 01:07:36 -0500

    Primer commit

---
+++ Farma.pe/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Farma.pe.Resources;
+
+namespace Farma.pe
+{
+    /// <summary>
+    /// Proporciona acceso a los recursos de cadena.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
